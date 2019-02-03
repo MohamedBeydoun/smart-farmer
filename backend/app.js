e3bc4260bce40a501011e06b06d493700f4c93dc
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-02-03 15:38:58 -0500

    setup express for backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const osrs = require("osrs-wrapper");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+	res.send("/ route working!");
+});
+
+app.listen(8081, () => {
+	console.log("listening on port 8081 ...")
+})
