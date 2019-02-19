0d145d7a66bf343ee6ee557cfb5e72bdf174a538
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-02-18 23:15:36 -0500

    farming route finised in backend

--- backend/app.js
+++ backend/app.js
@@ -9,14 +9,41 @@ app.get("/", (req, res) => {
 	res.send("/ route working!");
 });
 
-app.get("/farming", (req, res) => {
-	let snapPrice = osrs.ge.getItem("snapdragon seed")
-		.then(data => JSON.parse(data))
-		.then(data => data.item.current.price)
+app.get("/farming", async (req, res) => {
+	const snapSeedPrice = await osrs.ge.getItem("snapdragon seed")
+		.then(data => parseFloat(JSON.parse(data).item.current.price, 10))
 		.catch(err => {
 			console.log(err);
 		});
-	res.send();
+
+	const snapHerbPrice = await osrs.ge.getItem("snapdragon")
+		.then(data => parseFloat(JSON.parse(data).item.current.price, 10))
+		.catch(err => {
+			console.log(err);
+		});
+
+	const ranarrSeedPrice = await osrs.ge.getItem("ranarr seed")
+		.then(data => parseFloat(JSON.parse(data).item.current.price, 10))
+		.catch(err => {
+			console.log(err);
+		});
+
+	const ranarrHerbPrice = await osrs.ge.getItem("ranarr weed")
+		.then(data => parseFloat(JSON.parse(data).item.current.price, 10))
+		.catch(err => {
+			console.log(err);
+		});
+
+	const moneyPerSnapSeed = snapHerbPrice * 9;
+	const moneyPerRanarrSeed = ranarrHerbPrice * 9;
+	const snapProfit = parseInt((moneyPerSnapSeed - snapSeedPrice) * 1000);
+	const ranarrProfit = parseInt((moneyPerRanarrSeed - ranarrSeedPrice) * 1000);
+
+	const results = {
+		snapdragon: snapProfit,
+		ranarr: ranarrProfit
+	}
+	res.send(results);
 });
 
 app.listen(8081, () => {
