2b85ff19fc938d7c67ce4fdd2c707fe3668a2a96
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2019-02-18 22:20:11 -0500

    got ranarr data from api

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
 	res.send("/ route working!");
 });
 
+app.get("/farming", (req, res) => {
+	let snapPrice = osrs.ge.getItem("snapdragon seed")
+		.then(data => JSON.parse(data))
+		.then(data => data.item.current.price)
+		.catch(err => {
+			console.log(err);
+		});
+	res.send();
+});
+
 app.listen(8081, () => {
 	console.log("listening on port 8081 ...")
 })
